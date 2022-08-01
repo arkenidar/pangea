67332022e20c3ed2419e940f76740268218c6591
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-02 01:15:59 +0200

    the script adjusts itself

--- pang-cli.sh
+++ pang-cli.sh
@@ -1,3 +1,11 @@
 
-rlwrap lua /home/dario/Dropbox/main/code/lua/pangea/latest.lua $@
+###rlwrap lua /home/dario/Dropbox/main/code/lua/pangea/latest.lua $@
+
+###DIR=/home/dario/Dropbox/main/code/lua/pangea/
+DIR=$(dirname $0)/
+RUNNER=latest.lua
+#echo $DIR
+rlwrap lua $DIR$RUNNER $@
+
+# Geany config
 # sh /home/dario/Dropbox/main/code/lua/pangea/pang-cli.sh "%f" # in Geany/Execute
